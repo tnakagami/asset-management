b06abc9a96051a90181824737f8db772fbd59a07
tnakagami <dmroas.gqouni.ftloy1024@gmail.com>
2025-09-24 23:39:21 +0900

    Update dashboard Modify environment files

--- flower/entrypoint.sh
+++ flower/entrypoint.sh
@@ -16,7 +16,7 @@ readonly CELERY_CMD_WITH_OPTS="celery --app=config --workdir=/opt/app"
 # = Main loop =
 # =============
 is_waiting=1
-while [ ${is_waiting} -eq 1 ]; do
+while [ ${is_running} -eq 1 ] && [ ${is_waiting} -eq 1 ]; do
   sleep 1
   num_workers=$(${CELERY_CMD_WITH_OPTS} inspect active 2> /dev/null | grep -oE "worker.*?: OK" | wc -l)
 
