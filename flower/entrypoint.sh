5abd2d9397d3eb0cae2f5061ea46007a1cbefe56
tnakagami <dmroas.gqouni.ftloy1024@gmail.com>
2025-12-23 00:39:11 +0900

    Modify dashboard.html, docker-compose.yml, entrypoint.sh Add command.sh and custom-flower.js

--- flower/entrypoint.sh
+++ flower/entrypoint.sh
@@ -1,36 +1,11 @@
 #!/bin/bash
 
-is_running=1
+# Copy javascript file to static directory of flower
+to_path=$(find /usr/local -name flower.js)
+cp -f /opt/home/custom-flower.js ${to_path}
 
-# Setup handler
-handler(){
-  echo sigterm accepted
+# Change execution user
+su - user
 
-  is_running=0
-}
-trap handler 1 2 3 15
-
-readonly CELERY_CMD_WITH_OPTS="celery --app=config --workdir=/opt/app"
-
-# =============
-# = Main loop =
-# =============
-is_waiting=1
-while [ ${is_running} -eq 1 ] && [ ${is_waiting} -eq 1 ]; do
-  sleep 1
-  num_workers=$(${CELERY_CMD_WITH_OPTS} inspect active 2> /dev/null | grep -oE "worker.*?: OK" | wc -l)
-
-  if [ ${num_workers} -gt 0 ]; then
-    is_waiting=0
-  fi
-done
-# Run flower
-${CELERY_CMD_WITH_OPTS} flower --log_to_stderr &
-pid=$!
-
-while [ ${is_running} -eq 1 ]; do
-  sleep 1
-done
-
-# Finalize
-kill ${pid}
+# Execute command with arguments
+$@
