452311ed473d2e835a852a4548918fb1390ccbf0
tnakagami <dmroas.gqouni.ftloy1024@gmail.com>
2025-12-24 01:58:08 +0900

    Update Dockerfile and entrypoint.sh

--- flower/entrypoint.sh
+++ flower/entrypoint.sh
@@ -4,11 +4,5 @@
 to_path=$(find /usr/local -name flower.js)
 cp -f /opt/home/custom-flower.js ${to_path}
 
-# Install command
-   apk update \
-&& apk upgrade \
-&& apk add --no-cache su-exec tini \
-&& rm -rf /root/.cache /var/cache/apk/* /tmp/*
-
 # execute process by local user
 exec su-exec user /sbin/tini -e 143 -- "$@"
